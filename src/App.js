29cde9eef2f83ac8a01826f02c8b8bd7a14f5734
Mr-Azeez <114315712+Mr-Azeez@users.noreply.github.com>
2023-01-06 08:23:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import CounterTwo from './components/CounterTwo';
 
 class  App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <CounterTwo 
+        render={(count, incrementCount) => (
+        <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        ) } 
+      />
+
+        <CounterTwo 
+        render={(count, incrementCount) => (
+        <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        ) } 
+      />
+        {/* <ClickCounter2 />
+        <HoverCounter2 />
+        <User render ={(isLoggedIn) => isLoggedIn ? 'Azeez' : 'Guest' } /> */}
       </div>
     );
   }
